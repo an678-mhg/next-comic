03dc2e258a95707d2327932a8ef346a20b78e15f
an678-mhg <an567008@gmail.com>
2022-07-09 13:04:51 +0700

    fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: FC<HomeProps> = ({ data }) => {
         <Meta
           title="NextComics | Website đọc truyện tranh"
           description="Website được tạo bởi Nextjs và Reactjs"
-          image="https://scontent.fsgn13-2.fna.fbcdn.net/v/t1.15752-9/290717828_1072115733435959_6212475330637442786_n.png?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=pECgeCJ4hPEAX9ymckV&_nc_ht=scontent.fsgn13-2.fna&oh=03_AVJqXtEM8n6lT-nWd7vQYYq3iYeIYu1gr99bUvqb16Inxg&oe=62EE84BB"
+          image="https://res.cloudinary.com/annnn/image/upload/v1657346489/290717828_1072115733435959_6212475330637442786_n_k49hf0.png"
         />
         <div>
           {Object.keys(data).map((item) => (
