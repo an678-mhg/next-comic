814ec95798855cf800e992a7c84926ce0f954140
an678-mhg <an567008@gmail.com>
2022-07-10 11:08:53 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 1000,
+  staticPageGenerationTimeout: 2000,
 };
 
 module.exports = nextConfig;
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
   baseURL: `${process.env.BASE_URL}/v1`,
-  timeout: 20000,
+  timeout: 30000,
 });
 
 export default client;
